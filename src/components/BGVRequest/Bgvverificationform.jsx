8bfe7ba0f90fea161ab2005946066d9c3a895fde
chintu <chintuswain451@gmail.com>
2026-05-11 12:55:35 +0530

    made change bgv verification form jsx for css file

--- src/components/BGVRequest/Bgvverificationform.jsx
+++ src/components/BGVRequest/Bgvverificationform.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./BGVVerificationForm.css";
+import "./Bgvverificationform.css";
 import toast from "react-hot-toast";
 import axiosInstance from "../../api/axiosInstance";
 
